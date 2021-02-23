eda56a2d169f626b39aa2404ad6e5c4a686b052c
Gaj911 <71267614+Gaj911@users.noreply.github.com>
2021-02-23 19:01:07 +0100

    Add MiniPLane

--- planeticket/src/components/VolsAller/VolsAller.js
+++ planeticket/src/components/VolsAller/VolsAller.js
@@ -8,15 +8,15 @@ const VolsAller = ({vols}) => {
 
   return (
     <>
-        <div>    
-          <img src="miniPlane.jpg" alt="icon miniplane" height="20"/>
-          <p className={css.lign}>Aller :  {vols.AeroportDep + " à déstination de " + vols.AeroportArv}</p>
-        </div>
+          <div>    
+             <p className={css.lign}><img src="miniPlane.jpg" alt="icon miniplane" height="20"/> Aller :  {vols.AeroportDep + " à déstination de " + vols.AeroportArv}</p>
+          </div>
 
-        <div className={css.orange}>
-          <p>{vols.Date + " -> " + vols.Prix}</p>
-          <p>Dép {vols.HeureDep + " Arr " + vols.HeureArr}</p>
-       </div>
+          <div className={css.orange}>
+            <p>{vols.Date + " -> " + vols.Prix}</p>
+            <p>Dép {vols.HeureDep + " Arr " + vols.HeureArr}</p>
+        
+          </div>
     </>
   );
 };
